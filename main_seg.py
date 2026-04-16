2e65e35913724e71cdc2f1fce58b03711d989bcd
noorulain-nn <noor.ulain@isb.nu.edu.pk>
2026-04-16 10:26:37 +0500

    1 shot

--- main_seg.py
+++ main_seg.py
@@ -30,7 +30,7 @@ import Metrics
 VOC_ROOT            =  "./data/fss-data/VOCdevkit/VOC2012" #"C:\\data\\VOCdevkit\\VOC2012"             #"./data/fss-data/VOCdevkit/VOC2012" C:\data\sbd\benchmark_RELEASE/dataset
 SBD_ROOT            =  "./data/fss-data/sbd/benchmark_RELEASE/dataset"#"C:\\data\\sbd\\benchmark_RELEASE\\dataset"   
 NUM_FOLDS           = 4  # ← Run all 4 folds
-K_SHOT              = 5
+K_SHOT              = 1
 BACKBONE_NAME       = "resnet50"
 DECODER_CHANNELS    = 256   # FPN output channels — 256 is standard
 BATCH_SIZE          = 8
